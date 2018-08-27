00e16b105bec3e85682339ca61fe64b62cb07c2a
SasakiYuki <wacorggen@gmail.com>
2018-08-27 16:36:40 +0900

    キャッシュをしないようにする

--- app/src/main/java/com/example/yukisasakia/testtileview/TestActivity.kt
+++ app/src/main/java/com/example/yukisasakia/testtileview/TestActivity.kt
@@ -1,12 +1,12 @@
 package com.example.yukisasakia.testtileview
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-
+import android.widget.TextView
 import com.qozix.tileview.TileView
 
 class TestActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
@@ -15,11 +15,12 @@ class TestActivity : AppCompatActivity() {
 
     private fun setupTileView() {
         TileView.Builder(findViewById<TileView>(R.id.tileView))
-                .setSize(10240, 10240)
+                .setSize(5120,5120)
+                .setMemoryCacheSize(0)
                 .defineZoomLevel("tiles/tile1/400/%2\$d-%1\$d.png")
                 .defineZoomLevel(1, "tiles/tile1/200/%2\$d-%1\$d.png")
                 .defineZoomLevel(2, "tiles/tile1/100/%2\$d-%1\$d.png")
-                .defineZoomLevel(3, "tiles/tile1/50/%2\$d_%1\$d.png")
+                .defineZoomLevel(3,"tiles/tile1/50/%2\$d_%1\$d.png")
                 .build()
     }
 }
